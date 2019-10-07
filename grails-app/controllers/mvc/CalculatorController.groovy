6d0821cf833addcff06d6a48a3ad298667ad7981
Thomas Müller <thomas.mueller1@students.fhnw.ch>
2019-10-07 16:37:08 +0200

    Added Model to Controller

--- grails-app/controllers/mvc/CalculatorController.groovy
+++ grails-app/controllers/mvc/CalculatorController.groovy
@@ -2,8 +2,14 @@ package mvc
 
 class CalculatorController {
 
-    def calc(double en, double exam) {
-        double result = (en + exam) / 2
-        render view: "CalculatorOutput", model: [result: result]
+    def calc(CalculatorModel calculatorModel) {
+        calculatorModel.result = (calculatorModel.en + calculatorModel.exam) / 2
+        render view: "CalculatorOutput", model: [model: calculatorModel]
+    }
+
+    class CalculatorModel {
+        double en
+        double exam
+        double result
     }
 }
